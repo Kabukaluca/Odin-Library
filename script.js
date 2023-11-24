31285945cf51367f34dbd82b2c5b6256c5e3fdc4
Kabukaluca <130925727+Kabukaluca@users.noreply.github.com>
2023-11-25 00:27:30 +0100

    Display error-msg if form is not filled
    error message when trying to submit empty form will increase UX, as the User knows what he has to do;

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let content = document.querySelector(".content");
 /* ===== Constants ===== */
 
 const book1 = new Book("1984", "George Orwell", "328", true);
-const book2 = new Book("test", "by tester", "123", false)
+const book2 = new Book("Test but in a very long way to test u", "by tester", "123", false)
 const library = [book1, book2];
 const favoriteBooks = [book1];
 
@@ -13,7 +13,11 @@ const newBookForm = document.getElementById("add-book-form");
 const formOverlay = document.getElementById("form-overlay");
 const closeForm = document.getElementById("close-form");
 const newBookBtn = document.getElementById("new-book-btn");
-const addBookBtn = document.getElementById("add-book-btn");
+const createBookBtn = document.getElementById("add-book-btn");
+
+const errorTitle = document.getElementById("error-title");
+const errorAuthor = document.getElementById("error-author");
+const errorPages = document.getElementById("error-pages");
 
 
 /* ===== Functions ===== */
@@ -23,7 +27,7 @@ function Book(title, author, pages, read) {
     this.pages = pages;
     this.read = read;
     this.readStatus = function() {
-        const readStatus = this.read ? "already read" : "not read yet";
+        const readStatus = this.read ? "Already read" : "Not read yet";
     return `${readStatus}`;
     };
 };
@@ -42,34 +46,57 @@ function displayBooks(arr) {
     });
 
     function createCard(book) {
+        /* -- Card Layout -- */
         const card = document.createElement("div");
         card.classList.add("card");
 
+        const cardHeading = document.createElement("div");
+        cardHeading.classList.add("card-heading");
+
+        const cardContent = document.createElement("div");
+        cardContent.classList.add("card-content");
+
+        const cardBtn = document.createElement("div");
+        cardBtn.classList.add("card-btn");
+
+        /* -- Content Elements -- */
         const title = document.createElement("h2");
-        title.textContent = `${book.title}`;
+        title.textContent = `"${book.title}"`;
 
         const author = document.createElement("p");
-        author.textContent = `by ${book.author}`;
+        author.textContent = `${book.author}`;
 
         const pages = document.createElement("p");
-        pages.textContent = `${book.pages} pages`;
+        pages.textContent = `${book.pages} Pages`;
 
         const readStatus = document.createElement("button");
         readStatus.textContent = book.readStatus();
-        readStatus.setAttribute("class", "read-status-btn")
+        readStatus.classList.add(book.read ? "read" : "not-read")
 
         const remove = document.createElement("button");
         remove.textContent = `Remove`;
         remove.setAttribute("id", "book-remove-btn")
 
+        /*
+        const favorite = document.createElement("button");
+        favorite.textContent = `Favorite`
+        favorite.setAttribute("id", "favorite-btn") */
+
         card.setAttribute("data-index", library.indexOf(book));
 
-        card.appendChild(title);
-        card.appendChild(author);
-        card.appendChild(pages);
-        card.appendChild(readStatus);
-        card.appendChild(remove);
+        /* -- Append Elements -- */
+        cardHeading.appendChild(title);
+        cardContent.appendChild(author);
+        cardContent.appendChild(pages);
+        cardBtn.appendChild(readStatus);
+        cardBtn.appendChild(remove);
+        // cardBtn.appendChild(favorite);
 
+        card.appendChild(cardHeading);
+        card.appendChild(cardContent);
+        card.appendChild(cardBtn);
+        
+        /* -- Card Eventlisteners -- */
         remove.addEventListener("click", () => {
             let index = card.getAttribute("data-index");
             removeBook(index);
@@ -132,16 +159,26 @@ newBookBtn.addEventListener("click", () => {
     formOverlay.style.display = "flex";
 }); 
 
-addBookBtn.addEventListener("click", (e) => {
+createBookBtn.addEventListener("click", (e) => {
 
     let titleValue = document.getElementById("title").value;
     let authorValue = document.getElementById("author").value;
     let pagesValue = document.getElementById("pages").value;
     let readValue = document.getElementById("read").checked;
 
-    if (titleValue === "" || authorValue === "" || pagesValue === "") {
+    if (titleValue === ""){
         e.preventDefault();
-    } else {
+        errorTitle.textContent = "You forgot to enter the title!";
+    }
+    if (authorValue === "") {
+        e.preventDefault();
+        errorAuthor.textContent = "You forgot to enter the author!";
+    }
+    if (pagesValue === "") {
+        e.preventDefault();
+        errorPages.textContent = "You forgot to enter pages!";
+    } 
+    else {
     addBookToLibrary(titleValue, authorValue, pagesValue, readValue);
 
     document.getElementById("title").value = "";
@@ -150,9 +187,15 @@ addBookBtn.addEventListener("click", (e) => {
     document.getElementById("read").checked = "";
 
     formOverlay.style.display = "none";
-    }
+    errorTitle.textContent = "";
+    errorAuthor.textContent = "";
+    errorPages.textContent = "";
+    };
 });
 
 closeForm.addEventListener("click", () => {
     formOverlay.style.display = ("none");
+    errorTitle.textContent = "";
+    errorAuthor.textContent = "";
+    errorPages.textContent = "";
 });
